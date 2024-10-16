f02a05a4d1b7a8fb342aadbe417efbf62326c25f
diogoshindhinaka <diogoshindhinaka@gmail.com>
2024-10-16 14:56:42 +0000

    Add a responvidade nas abas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,9 +30,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <div className="grid min-h-screen grid-cols-app">
+        <div className="min-h-screen lg:grid lg:grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
+          <main className="max-w-screen px-4 pb-12 pt-24 lg:col-start-2 lg:px-8 lg:pt-8">
             {children}
           </main>
         </div>
